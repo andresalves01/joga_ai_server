a52a05a8ca7fb41e5d9d7e071d85edfe3ffc2849
andre <andresalves2001@gmail.com>
2023-12-31 20:27:58 -0300

    Amenity refactor

--- src/model/id/Amenity.py
+++ src/model/id/Amenity.py
@@ -1,7 +1,5 @@
 from typing import Any, Dict
 from .Model_ID import Model_ID
-from ..Model import Model
-import re
 
 
 class Amenity(Model_ID):
@@ -12,24 +10,63 @@ class Amenity(Model_ID):
         self.name = name
         self.icon_url = icon_url
 
-    def copy(self) -> Model:
+    def copy(self) -> "Amenity":
         return Amenity(self.schema, self.name, self.id, self.icon_url)
 
+    def from_dict(self, dictionary: dict[str, Any]) -> None:
+        super().from_dict(dictionary)
+        self.name = dictionary.pop("name", None)
+        self.icon_url = dictionary.pop("icon_url", None)
+
+    def to_dict(
+        self, ignore_none: bool = False, include_id: bool = False
+    ) -> dict[str, Any]:
+        self_dict = {"name": self.name, "icon_url": self.icon_url}
+
+        result = super().to_dict(ignore_none, include_id)
+        if ignore_none:
+            for key, value in self_dict.items():
+                if value is not None:
+                    result[key] = value
+
+        return result
+
     @property
-    def name(self) -> str:
-        return self.__name
+    def name(self) -> None | str:
+        return self._name
 
     @name.setter
-    def name(self, value: str) -> None:
-        self.__name = value
+    def name(self, name: None | str) -> None:
+        if name is None:
+            self._name = None
+            return
+
+        try:
+            name = str(name)
+            if len(name) > 50:
+                raise ValueError(
+                    f"Name length should be less than 50 characters, but it has {len(name)}"
+                )
+            self._name = name
+        except TypeError:
+            raise TypeError(f"Name should be a String, not a {type(name)}")
 
     @property
     def icon_url(self) -> str:
-        return self.__icon_url
+        return self._icon_url
 
     @icon_url.setter
-    def icon_url(self, value: str) -> None:
-        if value is None or (len(value) <= 2080):
-            self.__icon_url = value
-        else:
-            raise Exception("Invalid Icon Url")
+    def icon_url(self, icon_url: None | str) -> None:
+        if icon_url is None:
+            self._icon_url = None
+            return
+
+        try:
+            icon_url = str(icon_url)
+            if len(icon_url) > 2080:
+                raise ValueError(
+                    f"Icon Url length should be less than 2080 characters, but it has {len(icon_url)}"
+                )
+            self._icon_url = icon_url
+        except TypeError:
+            raise TypeError(f"Icon Url should be a String, not a {type(icon_url)}")
